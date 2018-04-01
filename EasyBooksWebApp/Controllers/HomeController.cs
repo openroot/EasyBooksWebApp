6c0be90c92212da3da6d4576685a4cc3f8c8d3f4
openroot <dev.openroot@gmail.com>
2018-04-02 00:28:13 +0530

    Updated About and Contact page contents.

--- EasyBooksWebApp/Controllers/HomeController.cs
+++ EasyBooksWebApp/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace EasyBooksWebApp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "EasyBooks application";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Feel free to contact at";
 
             return View();
         }
